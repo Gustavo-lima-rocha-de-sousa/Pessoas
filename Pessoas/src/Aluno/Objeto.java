52b59478b453e443a0c1c5a937ddf437fc9f9363
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-10-22 15:51:32 -0300

    HashMap

--- Pessoas/src/Aluno/Objeto.java
+++ Pessoas/src/Aluno/Objeto.java
@@ -1,6 +1,7 @@
 package Aluno;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JOptionPane;
@@ -12,9 +13,7 @@ public class Objeto {
 
 		List<Aluno> alunos = new ArrayList<Aluno>();
 
-		List<Aluno> alunosAprovado = new ArrayList<Aluno>();
-		List<Aluno> alunosRecuperacao = new ArrayList<Aluno>();
-		List<Aluno> alunosReprovado = new ArrayList<Aluno>();
+		HashMap<String, List<Aluno>> maps = new HashMap<String, List<Aluno>>();
 
 		for (int qtd = 1; qtd <= 5; qtd++) {
 
@@ -75,31 +74,39 @@ public class Objeto {
 			}
 			alunos.add(aluno1);
 		}
+		
+		maps.put(StatusAluno.APROVADO, new ArrayList<Aluno>());
+		maps.put(StatusAluno.RECUPERACAO, new ArrayList<Aluno>());
+		maps.put(StatusAluno.REPROVADO, new ArrayList<Aluno>());
+		
 		for (Aluno aluno : alunos) {
 			if (aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.APROVADO)) {
-				alunosAprovado.add(aluno);
+				maps.get(StatusAluno.APROVADO);
 
 			} else if (aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
-				alunosRecuperacao.add(aluno);
+				maps.get(StatusAluno.RECUPERACAO);
 
 			} else if (aluno.getAlunoAprovado2().equalsIgnoreCase(StatusAluno.REPROVADO)) {
-				alunosReprovado.add(aluno);
+				maps.get(StatusAluno.REPROVADO);
 			}
 		}
-		
+
 		System.out.println("------->Lista Aprovados<----------- ");
-		for (Aluno aluno : alunosAprovado) {
-			System.out.println(aluno.getNome() + " Resultado: " + aluno.getAlunoAprovado2() + " Com media de: " + aluno.getMediaNota());
+		for (Aluno aluno : maps.get(StatusAluno.APROVADO)){
+			System.out.println(aluno.getNome() + " Resultado: " + aluno.getAlunoAprovado2() + " Com media de: "
+					+ aluno.getMediaNota());
 		}
-		
+
 		System.out.println("------->Lista Recuperação<----------- ");
-		for (Aluno aluno : alunosRecuperacao) {
-			System.out.println(aluno.getNome() + " Resultado: " + aluno.getAlunoAprovado2() + " Com media de: " + aluno.getMediaNota());
-		}	
-			
+		for (Aluno aluno : maps.get(StatusAluno.RECUPERACAO)) {
+			System.out.println(aluno.getNome() + " Resultado: " + aluno.getAlunoAprovado2() + " Com media de: "
+					+ aluno.getMediaNota());
+		}
+
 		System.out.println("------->Lista Reprovados<----------- ");
-		for (Aluno aluno : alunosReprovado) {
-			System.out.println(aluno.getNome() + " Resultado: " + aluno.getAlunoAprovado2() + " Com media de: " + aluno.getMediaNota());
-		}	
+		for (Aluno aluno : maps.get(StatusAluno.REPROVADO)) {
+			System.out.println(aluno.getNome() + " Resultado: " + aluno.getAlunoAprovado2() + " Com media de: "
+					+ aluno.getMediaNota());
+		}
 	}
 }
