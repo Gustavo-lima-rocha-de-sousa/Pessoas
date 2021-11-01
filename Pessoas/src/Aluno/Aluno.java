7043b88436bc066d27f7b2c2c872f1392ae01620
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-01 14:41:50 -0300

    Herança

--- Pessoas/src/Aluno/Aluno.java
+++ Pessoas/src/Aluno/Aluno.java
@@ -7,19 +7,11 @@ import cursojava.constantes.StatusAluno;
 
 import java.util.List;
 
-public class Aluno {
+public class Aluno extends Pessoa {
 	/* Esses são os atributos do Aluno */
-	private String nome;
-	private int idade;
-	private String dataNascimento;
-	private String registroGeral;
-	private String numeroCpf;
-	private String nomeMae;
-	private String nomePai;
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
-
 	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
 
 	public void setDisciplinas(List<Disciplina> disciplinas) {
