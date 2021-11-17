e62412ef5f053e7df8aa51e088149bec069a8226
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-17 11:18:17 -0300

    Criando nosso primeiro Array.(Curso FullStack Java Web.

---
+++ Pessoas/src/curso/java/excecao/ArrayVetor.java
@@ -0,0 +1,21 @@
+package curso.java.excecao;
+
+public class ArrayVetor {
+	
+	/*Usado para executar código*/
+	public static void main(String[] args) {
+		
+		/*Array pode ser de todos os tipos de dados e objetos também*/
+		
+	/*array sempre deve ter a quantiadade de posições definidas*/	
+	double[] notas = new double[4];
+	
+	/*Atribuir valor nas posições do array*/
+	notas [0] = 7.8;
+	notas [1] = 8.7;
+	notas [2] = 9.7;
+	notas [3] = 9.9;
+	
+		System.out.println(notas);
+	}
+}
