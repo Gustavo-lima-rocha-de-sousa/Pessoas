2dbe8147923261c08fef34195913f773b1443c52
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-25 17:32:01 -0300

    Conversão Array para Lista e Lista para Array (Curso Java Web FullStack).

--- Pessoas/src/curso/java/excecao/SplitArray.java
+++ Pessoas/src/curso/java/excecao/SplitArray.java
@@ -1,5 +1,8 @@
 package curso.java.excecao;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 
 	public static void main(String[] args) {
@@ -14,5 +17,21 @@ public class SplitArray {
 		System.out.println("Nota2: " + valoreArray[3]);
 		System.out.println("Nota3: " + valoreArray[4]);
 		System.out.println("Nota4: " + valoreArray[5]);
+		
+		/*Convertendo um Array em uma Lista*/
+		
+		List<String> list = Arrays.asList(valoreArray);
+		
+		for (String valoString : list) {
+			System.out.println(valoString);
+		}
+		
+		/*Converter uma Lista para Array*/
+		String[]conversaoArray = list.toArray(new String[6]);
+		
+		for(int pos = 0; pos < conversaoArray.length; pos++) {
+			System.out.println(conversaoArray[pos]);
+			
+		}
 	}
 }
