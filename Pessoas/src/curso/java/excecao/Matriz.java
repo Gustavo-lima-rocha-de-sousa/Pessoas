606f8444b91b9a1e77c5c1f19c15379ff798d3c9
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-25 15:59:29 -0300

    Criando uma Matriz multidimensional(Curso Java Web FullStack).

---
+++ Pessoas/src/curso/java/excecao/Matriz.java
@@ -0,0 +1,20 @@
+package curso.java.excecao;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		
+		int notas[][] = new int[2][3];
+		
+		notas[0][0] = 80;
+		notas[0][1] = 90;
+		notas[0][2] = 70; 
+		
+		notas[1][0] = 5;
+		notas[1][1] = 70;	
+		notas[1][2] = 8;
+		
+		System.out.println(notas);
+		
+	}
+}
