9cbaf849b6b21ead47612728252758fb703b87a8
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-26 15:47:57 -0300

    Criando a primeira Thread (Curso Java Web FullStack Java).

--- Pessoas/src/cursojava/thread/AulaThread.java
+++ Pessoas/src/cursojava/thread/AulaThread.java
@@ -1,19 +1,36 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		
-	
-		for (int pos = 0; pos < 10; pos ++){
+		/*Thread procesando em paralelo*/
+		new Thread() {
 			
-			/* Quero Executar essa envio de e-mail com um tempo de para ou tempo determinado */
-			System.out.println("Executando alguma rotina, por exemplo envio de e-mail.");
-			Thread.sleep(1000);
-			System.out.println("Aguarde, estamos trabalhando nisso.");
-		} 
-			System.out.println("E-mail enviado");			
+			public void run()/*Executa o que queremos*/ {
+				/*Código da rotina*/
+				for (int pos = 0; pos < 10; pos ++){
+				/* Quero Executar essa envio de e-mail com um tempo de para ou tempo determinado */
+				System.out.println("Executando alguma rotina, por exemplo envio de e-mail.");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
+				
+				} 
+				/*Fim do código em paralelo*/
+					
+				};
+			}.start(); /*Liga a Thread que fica processando de forma paralela por trás*/
 		
+			
+		/*Código do sistema do usuário continua o fluxo de trabalho*/
+		System.out.println("E-mail enviado");			
+		/*Fluxo do sistema, cadastro de venda, emissao de nota fiscal, algo do tipo*/
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
 }
