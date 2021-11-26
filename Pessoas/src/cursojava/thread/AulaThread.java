82e72b787f97fa591b608b2cecd911d9f58ad30f
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-26 15:59:21 -0300

    Processamento concorrente entre duas Thread (Curso Java Web FullStack Java).

--- Pessoas/src/cursojava/thread/AulaThread.java
+++ Pessoas/src/cursojava/thread/AulaThread.java
@@ -6,7 +6,7 @@ public class AulaThread {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		/*Thread procesando em paralelo*/
+		/*Thread procesando em paralelo envio do e-mail*/
 		new Thread() {
 			
 			public void run()/*Executa o que queremos*/ {
@@ -29,11 +29,36 @@ public class AulaThread {
 					
 				};
 			}.start(); /*Liga a Thread que fica processando de forma paralela por trás*/
-		
+		/*------------------------------ Divisão das Threads*/
+			/*Thread procesando em paralelo envio da nota fiscal*/
+			new Thread() {
+				
+				public void run()/*Executa o que queremos*/ {
+					/*Código da rotina*/
+					for (int pos = 0; pos < 10; pos ++){
+					/* Quero Executar essa envio de nota fiscal com um tempo de para ou tempo determinado */
+					System.out.println("Executando alguma rotina, por exemplo envio de Nota Fiscal.");
+					
+					try {
+						Thread.sleep(1000);
+						
+					} catch (InterruptedException e) {
+						
+						e.printStackTrace();
+						
+					}
+					
+					} 
+					/*Fim do código em paralelo*/
+						
+					};
+				}.start(); /*Liga a Thread que fica processando de forma paralela por trás*/	
+			
 			
 		/*Código do sistema do usuário continua o fluxo de trabalho*/
 					
 		/*Fluxo do sistema, cadastro de venda, emissao de nota fiscal, algo do tipo*/
+			
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
 }
