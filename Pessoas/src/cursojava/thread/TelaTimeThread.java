d08ba8dbd9384bc0e4b1c67379253c687b48eba5
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-26 17:04:41 -0300

    Conhecendo o GridBagConstraints gerenciador de posicionamento e layout (Curso Java Web FullStack).

--- Pessoas/src/cursojava/thread/TelaTimeThread.java
+++ Pessoas/src/cursojava/thread/TelaTimeThread.java
@@ -1,13 +1,27 @@
 package cursojava.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
 	
+	private JPanel jPanel = new JPanel(new GridBagLayout());/* Painel de Componentes*/
 	
-public TelaTimeThread() {/*Executa o que tem dentro no momento da abertura ou execução*/
+	private JLabel descricaoHora = new JLabel("Time Thread1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread2");
+	private JTextField mostraTempo2 = new JTextField();
+	
+	
+	public TelaTimeThread() {/*Executa o que tem dentro no momento da abertura ou execução*/
 	setTitle("Minha tela de time com Thread");
 	setSize(new Dimension(260, 260));
 	setLocationRelativeTo(null);
@@ -16,7 +30,29 @@ public TelaTimeThread() {/*Executa o que tem dentro no momento da abertura ou ex
 	
 	
 	
+	GridBagConstraints gridBagConstraints = new GridBagConstraints();/*Controlador de posicionamento de componentes*/
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 0;
 	
+	descricaoHora.setPreferredSize(new Dimension(200, 25));
+	jPanel.add(descricaoHora,gridBagConstraints);
+	
+	mostraTempo.setPreferredSize(new Dimension(200, 25));
+	gridBagConstraints.gridy ++;
+	jPanel.add(mostraTempo, gridBagConstraints);
+	
+	descricaoHora2.setPreferredSize(new Dimension(200,25));
+	gridBagConstraints.gridy++;
+	jPanel.add(descricaoHora2,gridBagConstraints);
+	
+	mostraTempo2.setPreferredSize(new Dimension(200, 25));
+	gridBagConstraints.gridy++;
+	jPanel.add(mostraTempo2, gridBagConstraints);
+	
+	
+	
+	
+	add(jPanel, BorderLayout.WEST);
 	/*Sempre sera o ultimo comando*/
 	setVisible(true);/*Torna a tela visivel para o usuario*/
 	}
