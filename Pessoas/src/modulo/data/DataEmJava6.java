ee11753993ff65430611d6a0292aa169071b110a
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-29 17:45:08 -0300

    Dia, meses, anos e semanas com a Nova API (Curso Java Web FullStack).

---
+++ Pessoas/src/modulo/data/DataEmJava6.java
@@ -0,0 +1,31 @@
+package modulo.data;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DataEmJava6 {
+
+	public static void main(String[] args) throws ParseException {
+	
+		LocalDate localDate = LocalDate.now();
+		System.out.println("Data Atual: " + localDate.format
+				(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println("Dia da semana: " + 
+		localDate.getDayOfWeek().name());
+		
+		System.out.println("Dia do mês: " + 
+				localDate.getDayOfMonth());
+		
+		System.out.println("Dia do ano: " + 
+				localDate.getDayOfYear());
+		
+		System.out.println("Mês: " + 
+				localDate.getMonth());
+		
+		System.out.println("Ano: " + 
+				localDate.getYear());
+		
+	}
+}
