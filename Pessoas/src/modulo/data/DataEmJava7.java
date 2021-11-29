9eacb6195f1e5c1bff9dda2757dc249f79895060
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-11-29 18:07:02 -0300

    Tempo de processo com Instant (Curso FullStack Java Web).

---
+++ Pessoas/src/modulo/data/DataEmJava7.java
@@ -0,0 +1,31 @@
+package modulo.data;
+
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.Instant;
+
+public class DataEmJava7 {
+
+	public static void main(String[] args) throws ParseException, InterruptedException {
+	
+		Instant inicio = Instant.now();
+		
+		Thread.sleep(2000); /*Processo com tempo que não conhecemos*/ 
+		
+		Instant ifinal = Instant.now();
+		
+		Duration duracao = Duration.between(inicio, ifinal);
+		
+		System.out.println("Duração em nano segundos: " + duracao.toNanos());
+		
+		System.out.println("Duração em minutos: " + duracao.toMinutes());
+		
+		System.out.println("Duração em horas: " + duracao.toHours());
+		
+		System.out.println("Duração em segundos: " + duracao.toSeconds());
+		
+		System.out.println("Duração em milisegundos: " + duracao.toMillis());
+		
+		System.out.println("Duração em dias: " + duracao.toDays());
+	}
+}
