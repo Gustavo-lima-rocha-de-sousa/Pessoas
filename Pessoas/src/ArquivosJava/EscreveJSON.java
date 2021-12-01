22b70c1a7e9558a69a43a3524404a4a3e784ac53
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-12-01 17:03:17 -0300

    Lendo arquivo JSON e montando Lista em Java (Cuso Java Web FullStack).

--- Pessoas/src/ArquivosJava/EscreveJSON.java
+++ Pessoas/src/ArquivosJava/EscreveJSON.java
@@ -1,11 +1,15 @@
 package ArquivosJava;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import Aluno.Usuario;
 
@@ -42,6 +46,24 @@ public class EscreveJSON {
 		fileWriter.write(jsonUser);
 		fileWriter.flush();
 		fileWriter.close();
+		
+		/*------Lendo o arquivo JSON------------*/
+		
+		FileReader fileReader = new FileReader
+				("C:\\Users\\dougl\\git\\Pessoas\\Pessoas\\src\\ArquivosJava\\filjson.json");
+		
+		JsonArray jsonArray =(JsonArray) JsonParser.parseReader(fileReader);
+		
+		List<Usuario> listUsuarios = new ArrayList<Usuario>();
+		
+		for (JsonElement jsonElement : jsonArray) {
+			
+			Usuario usuario = new Gson().fromJson(jsonElement, Usuario.class);
+			listUsuarios.add(usuario);
+		}
+		
+		System.out.println("Leitura do Arquivo JSON: " + listUsuarios );
+		
 	}
 
 }
